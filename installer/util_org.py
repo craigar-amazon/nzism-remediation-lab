76ce2f9c65019c49b0505f33663dc01a353fb0d1
CraigR <craigar@amazon.com>
2021-10-19 23:56:53 +1300

    LZ Navigator

--- installer/util_org.py
+++ installer/util_org.py
@@ -2,7 +2,7 @@ import sys
 import botocore
 import boto3
 
-def describeOrganization():
+def describe_organization():
     try:
         org_client = boto3.client('organizations')
         response = org_client.describe_organization()
@@ -13,7 +13,7 @@ def describeOrganization():
         print(e)
         return None
 
-def listRoots():
+def map_root_ids_by_name():
     rootIdByName = {}
     try:
         org_client = boto3.client('organizations')
@@ -31,7 +31,7 @@ def listRoots():
         print(e)
         return None
 
-def listOrganizationalUnits(parentId):
+def map_ou_ids_by_name(parentId):
     ouIdByName = {}
     try:
         org_client = boto3.client('organizations')
@@ -49,7 +49,7 @@ def listOrganizationalUnits(parentId):
         print(e)
         return None
 
-def listActiveCreatedOrganizationalAccounts(parentId):
+def map_account_ids_by_name(parentId, activeOnly=True, createdOnly=True):
     accountIdByName = {}
     try:
         org_client = boto3.client('organizations')
@@ -60,50 +60,105 @@ def listActiveCreatedOrganizationalAccounts(parentId):
             for account in accounts:
                 accountName = account['Name']
                 accountId = account['Id']
-                if (account['Status'] == 'ACTIVE') and (account['JoinedMethod'] == 'CREATED'):
-                    accountIdByName[accountName] = accountId
+                if activeOnly and (account['Status'] != 'ACTIVE'):
+                    continue
+                if createdOnly and (account['JoinedMethod'] != 'CREATED'):
+                    continue
+                accountIdByName[accountName] = accountId
         return accountIdByName
     except botocore.exceptions.ClientError as e:
         print("Failed to organizations.list_accounts_for_parent")
         print(e)
         return None
 
-rootName = 'Root'
-securityOUName = 'Security'
-auditAccountName = 'Audit'
-logArchiveAccountName = 'Log Archive'
-excludedOUNames = ['Legacy-Production']
+def map_account_names_by_id_recursive(parentId, excludedOUSet, activeOnly=True, createdOnly=True):
+    accountNameById = {}
+    accountIdByName = map_account_ids_by_name(parentId, activeOnly, createdOnly)
+    for accountName in accountIdByName:
+        accountId = accountIdByName[accountName]
+        accountNameById[accountId] = accountName
+    ouIdsByName = map_ou_ids_by_name(parentId)
+    for ouName in ouIdsByName:
+        if ouName in excludedOUSet:
+            continue
+        ouId = ouIdsByName[ouName]
+        accountNameByIdSub = map_account_names_by_id_recursive(ouId, excludedOUSet, activeOnly, createdOnly)
+        for accountIdSub in accountNameByIdSub:
+            accountNameById[accountIdSub] = accountNameByIdSub[accountIdSub]
+    return accountNameById
 
 
-org = describeOrganization()
-if not org:
-    raise Exception("Cannot continue without Organization information")
 
-parentId = org['Id']
-print("Organization Id: "+org['Id'])
-print("Management Account: "+org['MasterAccountId'] + " " + org['MasterAccountEmail'])
+def describeLandingZone(excludedOUNames=[], securityOUName='Security', auditAccountName='Audit', logArchiveAccountName='Log Archive', rootName='Root'):
+    lz = {}
+    lz['ExcludedOUNames'] = excludedOUNames
+    mgmt = describe_organization()
+    if not mgmt:
+        raise Exception("Cannot determine Organization information")
+    lz['OrganizationId'] = mgmt['Id']
+    lz['MasterAccountId'] = mgmt['MasterAccountId']
+    lz['MasterAccountEmail'] = mgmt['MasterAccountEmail']
+    rootIdsByName = map_root_ids_by_name()
+    if not (rootName in rootIdsByName):
+        print("Id for named Organization Root not found")
+        print("rootName: "+rootName)
+        raise Exception("Cannot determine Organization Root Id. Ensure account is delegated administrator")
+    rootId = rootIdsByName[rootName]
+    lz['RootName'] = rootName
+    lz['RootId'] = rootId
 
-rootIdByName = listRoots()
-if not (rootName in rootIdByName):
-    raise Exception("Cannot continue without Organization Root Id: Ensure account is delegated administrator")
+    ouIdsByName = map_ou_ids_by_name(rootId)
+    if not (securityOUName in ouIdsByName):
+        print("Security OU for Landing Zone not found")
+        print("Security OU Name: "+securityOUName)
+        raise Exception("Cannot find required '"+securityOUName+"' OU (Security). Ensure landing zone has been initialised")
+    securityOUId = ouIdsByName[securityOUName]
+    lz['SecurityOUName'] = securityOUName
+    lz['SecurityOUId'] = securityOUId
 
-rootId = rootIdByName[rootName]
+    securityAccountIdsByName = map_account_ids_by_name(securityOUId)
+    if not (auditAccountName in securityAccountIdsByName):
+        print("Audit Account for Landing Zone not found")
+        print("Audit Account Name: "+auditAccountName)
+        raise Exception("Cannot find required '"+auditAccountName+"' account (Audit). Ensure landing zone has been initialised")
+    lz['AuditAccountName'] = auditAccountName
+    lz['AuditAccountId'] = securityAccountIdsByName[auditAccountName]
 
-parentId = rootId
-excludedSet = set(excludedOUNames)
-ouMap = listOrganizationalUnits(parentId)
-if securityOUName in ouMap:
-    securityAccountMap = listActiveCreatedOrganizationalAccounts(ouMap[securityOUName])
-    if logArchiveAccountName in securityAccountMap:
-        logArchiveAccountId = securityAccountMap[logArchiveAccountName]
-    if auditAccountName in securityAccountMap:
-        auditAccountId = securityAccountMap[auditAccountName]
-for ouName in ouMap:
-    if ouName == securityOUName:
-        continue
-    if ouName in excludedSet:
-        continue
-    childAccountMap = listActiveCreatedOrganizationalAccounts(ouMap[ouName])
-    print(childAccountMap)
-   
+    if not (logArchiveAccountName in securityAccountIdsByName):
+        print("Log Archive Account for Landing Zone not found")
+        print("Log Archive Account Name: "+logArchiveAccountName)
+        raise Exception("Cannot find required '"+logArchiveAccountName+"' account (Log Archive). Ensure landing zone has been initialised")
+    lz['LogArchiveAccountName'] = logArchiveAccountName
+    lz['LogArchiveAccountId'] = securityAccountIdsByName[logArchiveAccountName]
 
+    secNode = {}
+    for securityAccountName in securityAccountIdsByName:
+        if securityAccountName == auditAccountName:
+            continue
+        if securityAccountName == logArchiveAccountName:
+            continue
+        secAccountId = securityAccountIdsByName[securityAccountName]
+        secNode[secAccountId] = securityAccountName
+    lz['SecuritySharedAccountsById'] = secNode
+
+    lzFactoryMembersByOUId = {}  
+    excludedOUNameSet = set(excludedOUNames)
+    for ouName in ouIdsByName:
+        if ouName == securityOUName:
+            continue
+        if ouName in excludedOUNameSet:
+            continue
+        ouId = ouIdsByName[ouName]
+        accountNameById = map_account_names_by_id_recursive(ouId, excludedOUNameSet, True, True)
+        ouNode = {}
+        ouNode['OUName'] = ouName
+        ouNode['OUAccountsById'] = accountNameById
+        lzFactoryMembersByOUId[ouId] = ouNode
+    lz['FactoryMembersByOUId'] = lzFactoryMembersByOUId
+    return lz  
+
+# lz1 = describeLandingZone(['Legacy-Production'])
+# print(lz1)
+
+lz1 = describeLandingZone([], 'core', 'security', 'log-archive')
+print(lz1)
